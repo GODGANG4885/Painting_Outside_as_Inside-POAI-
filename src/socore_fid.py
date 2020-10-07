755fd250be2c0499427b37418af090027d4829c7
Godgang <36159663+GODGANG4885@users.noreply.github.com>
2020-10-07 16:11:02 +0900

    Delete socore_fid.py

--- src/socore_fid.py
+++
@@ -1,8 +0,0 @@
-from score_infinity import calculate_FID_infinity, calculate_IS_infinity
-
-generator = '/home/godgang/edge-connect/examples/eval_test/result/hinge_LRmore/InpaintingModel_gen.pth'
-
-FID_infinity = calculate_FID_infinity(generator, 64, 32, gt_path='/home/godgang/NS-Outpainting/logs/0817/2/origval/statistics.npz')
-IS_infinity = calculate_IS_infinity(generator, 64, 32)
-
-print("FID_infinity : {} / IS_infinity : {}".format(FID_infinity,IS_infinity))
